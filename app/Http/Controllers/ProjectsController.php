8f344d37eecfc464c4ce78a86eb4912067b7ccd9
Artiom <sinigur2006@gmail.com>
2019-12-01 23:04:36 -0500

    Validate form fields for new project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -30,7 +30,11 @@ class ProjectsController extends Controller
      */
     public function store()
     {
-        Project::create(request(['title', 'description']));
+        $validateProject = request()->validate([
+            'title'     => ['required', 'min:3', 'max:50'],
+            'description'     => ['required', 'min:5', 'max:500']
+        ]);
+        Project::create($validateProject);
 
         return redirect('/projects');
     }
