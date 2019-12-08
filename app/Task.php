9ee9dd7b1dc1ca647cc4e209781af9b5950f3bd3
Artiom <sinigur2006@gmail.com>
2019-12-07 23:06:37 -0500

    Refactory task records activity with method boot

--- app/Task.php
+++ app/Task.php
@@ -8,8 +8,28 @@ class Task extends Model
 {
     protected $fillable = ['completed', 'description', 'project_id'];
 
-    // When a task is updated we need that project update too
+    // When a task is updated we need that project time update too
     protected $touches = ['project'];
+
+    public static function boot() {
+        parent::boot();
+
+        // Creating a new task records project activity
+        static::created(function ($task) {
+            Activity::create([
+                'project_id' => $task->project->id,
+                'description' => 'Task created'
+            ]);
+        });
+
+        // Updating a task records project activity
+        static::updated(function ($task) {
+            Activity::create([
+                'project_id' => $task->project->id,
+                'description' => 'Task completed'
+            ]);
+        });
+    }
 
     /**
      * Task belongs to a project
